6d99792b5548b92f15ffa10b2e13195e667ffe81
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2024-10-18 18:10:29 +0530

    routing added for add visit entry

--- src/app/modules/secure/visit-entry-list/visit-entry-list.component.ts
+++ src/app/modules/secure/visit-entry-list/visit-entry-list.component.ts
@@ -7,6 +7,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-visit-entry-list',
@@ -19,7 +20,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
-    MatSelectModule
+    MatSelectModule,
+    RouterModule
   ],
   templateUrl: './visit-entry-list.component.html',
   styleUrls: ['./visit-entry-list.component.scss']
