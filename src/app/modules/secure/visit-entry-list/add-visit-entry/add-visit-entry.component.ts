743e56a8ff942c6990262561865347ef09821096
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2024-10-18 18:19:48 +0530

    add visit entry page designed

--- src/app/modules/secure/visit-entry-list/add-visit-entry/add-visit-entry.component.ts
+++ src/app/modules/secure/visit-entry-list/add-visit-entry/add-visit-entry.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-add-visit-entry',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+  ],
   templateUrl: './add-visit-entry.component.html',
   styleUrls: ['./add-visit-entry.component.scss']
 })
