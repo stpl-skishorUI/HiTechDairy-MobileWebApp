028d0c95ccf65d2b6e7b499fd579bd2fbe023d45
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2024-10-18 17:57:48 +0530

    visit entry list page designed

--- src/app/modules/secure/visit-entry-list/visit-entry-list.component.ts
+++ src/app/modules/secure/visit-entry-list/visit-entry-list.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-visit-entry-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule
+  ],
   templateUrl: './visit-entry-list.component.html',
   styleUrls: ['./visit-entry-list.component.scss']
 })
