ae8c7a99bbb5d56a5a197ca77dbe4bb1626f1999
Shital Kirolkar <shital.kirolkar@shauryatechnosoft.com>
2024-10-22 15:32:38 +0530

    environment created for API

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+
+  //Demo
+  priyadarshaniService: 'https://priyadarshaniapi.mahamining.com/',
+  hrmsDairyService: 'https://demohrmsapi.hitechdairy.in/'
+
+  //UAT
+  // priyadarshaniService: 'http://demouat-api.hitechdairy.in/',
+  // hrmsDairyService: 'http://hrmsapiuat.hitechdairy.in/'
+
+  //Live
+  // priyadarshaniService: 'https://demouatapipriyadarshani.hitechdairy.in/',
+  // hrmsDairyService: 'https://hrms-api-prod.hitechdairy.in/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  
+  //Demo
+  priyadarshaniService: 'https://priyadarshaniapi.mahamining.com/',
+  hrmsDairyService: 'https://demohrmsapi.hitechdairy.in/'
+
+  //UAT
+  // priyadarshaniService: 'http://demouat-api.hitechdairy.in/',
+  // hrmsDairyService: 'http://hrmsapiuat.hitechdairy.in/'
+
+  //Live
+  // priyadarshaniService: 'https://demouatapipriyadarshani.hitechdairy.in/',
+  // hrmsDairyService: 'https://hrms-api-prod.hitechdairy.in/'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
