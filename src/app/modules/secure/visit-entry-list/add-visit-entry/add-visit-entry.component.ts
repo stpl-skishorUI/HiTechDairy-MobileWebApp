9b85cbbcba9ceeb8d1727b07ea26b1f3e5ced8dd
Shital Kirolkar <shital.kirolkar@shauryatechnosoft.com>
2024-10-22 15:33:32 +0530

    visit entry changes

--- src/app/modules/secure/visit-entry-list/add-visit-entry/add-visit-entry.component.ts
+++ src/app/modules/secure/visit-entry-list/add-visit-entry/add-visit-entry.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -7,6 +7,14 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { FormBuilder, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ReplaySubject } from 'rxjs';
+import { MasterService } from 'src/app/core/services/master.service';
+import { ApiService } from 'src/app/core/services/api.service';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
+import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
+import { ErrorService } from 'src/app/core/services/error.service';
 
 @Component({
   selector: 'app-add-visit-entry',
@@ -20,10 +28,146 @@ import { MatSelectModule } from '@angular/material/select';
     MatInputModule,
     MatDatepickerModule,
     MatSelectModule,
+    NgxMatSelectSearchModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   templateUrl: './add-visit-entry.component.html',
   styleUrls: ['./add-visit-entry.component.scss']
 })
 export class AddVisitEntryComponent {
 
+  registerForm!:FormGroup;
+  get f(){ return this.registerForm.controls};
+  organizationArray = new Array();
+  orgCtrl: FormControl = new FormControl();
+  orgSubject: ReplaySubject<any> = new ReplaySubject<any>();
+  unitArray = new Array();
+  unitCtrl: FormControl = new FormControl();
+  unitSubject: ReplaySubject<any> = new ReplaySubject<any>();
+  toUnitArray = new Array();
+  toUnitCtrl: FormControl = new FormControl();
+  toUnitSubject: ReplaySubject<any> = new ReplaySubject<any>();
+  employeeArray = new Array();
+  employeeCtrl: FormControl = new FormControl();
+  employeeSubject: ReplaySubject<any> = new ReplaySubject<any>();
+  editObj:any;
+  editFlag:boolean=false;
+  @ViewChild('formDirective') private formDirective!: NgForm;
+  constructor( private fb:FormBuilder, public webService:WebStorageService, public commonService:CommonMethodsService,
+    private masterService:MasterService, private apiService:ApiService, 
+    private errorService:ErrorService
+  ){
+
+  }
+  ngOnInit(){
+    this.getFormControls();
+    this.searchDataZone();
+    !this.editObj ? this.getOrganation() : '';
+  }
+
+  getFormControls(data?:any){
+    this.registerForm = this.fb.group({
+      organizationId: [data? data?.organizationId : '',[Validators.required]],
+      unitId: [data? data?.toUnitId : '',[Validators.required]],
+      fromUnitId: [data? data?.fromUnitId :'',[Validators.required]],
+      employeeId: [data? data?.partyId :'',[Validators.required]],
+      visitDate: [data? new Date(data?.visitDate) :new Date(),[Validators.required]],
+      visitTime: [data ? data?.visitTime: this.commonService.getTimeFromDateTime(new Date()),[Validators.required]],
+      visitDetails: [data? data?.visitDetails :''],
+    })
+  }
+
+  searchDataZone() {  //used for serachable dropdown when we reload the page
+    this.orgCtrl.valueChanges.pipe().subscribe(() => { this.commonService.filterArrayDataZone(this.organizationArray,this.orgCtrl,'organizationName',this.orgSubject) });
+    this.unitCtrl.valueChanges.pipe().subscribe(() => { this.commonService.filterArrayDataZone(this.unitArray,this.unitCtrl,'unitName',this.unitSubject) });
+    this.toUnitCtrl.valueChanges.pipe().subscribe(() => { this.commonService.filterArrayDataZone(this.toUnitArray,this.toUnitCtrl,'unitName',this.toUnitSubject) });
+    this.employeeCtrl.valueChanges.pipe().subscribe(() => { this.commonService.filterArrayDataZone(this.employeeArray,this.employeeCtrl,'employeeName',this.employeeSubject) });
+  }
+
+  getOrganation() {
+    this.organizationArray = [];
+    this.masterService.getAllOrganizationByUserId(this.webService.getUserId()).subscribe({
+      next: ((res: any) => {
+        if (res.statusCode == "200") {
+          this.organizationArray = res.responseData;
+          this.commonService.filterArrayDataZone(this.organizationArray,this.orgCtrl,'organizationName',this.orgSubject);
+          this.webService.getOrgId() || this.editFlag ? (this.f['organizationId'].setValue(this.editObj?.organizationId || this.webService.getOrgId()),this.getUnit(), this.getEmployees()):'';
+        }
+        else {
+          this.organizationArray = [];
+        }
+      }),
+    })
+  }
+  getUnit(){
+    this.masterService.getAllUnitByOrgIdUserId(this.f['organizationId']?.getRawValue(), this.webService.getUserId()).subscribe({
+      next: ((res: any) => {
+        if (res.statusCode == "200") {
+          this.unitArray = res.responseData;
+          this.toUnitArray = res.responseData;
+          this.commonService.filterArrayDataZone(this.unitArray, this.unitCtrl, 'unitName', this.unitSubject);
+          this.commonService.filterArrayDataZone(this.toUnitArray,this.toUnitCtrl,'unitName',this.toUnitSubject) ;
+        }
+        else {
+          this.unitArray = [];
+          this.toUnitArray = [];
+        }
+      }),
+    })
+  }
+  getEmployees(){
+    this.masterService.getAllEmployeeByOrg(this.f['organizationId']?.getRawValue()).subscribe({
+      next: ((res: any) => {
+        if (res.statusCode == "200") {
+          this.employeeArray = res.responseData;
+          this.commonService.filterArrayDataZone(this.employeeArray,this.employeeCtrl,'employeeName',this.employeeSubject);
+        }
+        else {
+          this.employeeArray = [];
+        }
+      }),
+    })
+  }
+  onSubmit(){
+    this.registerForm.updateValueAndValidity();
+    if(this.registerForm.invalid){
+      return;
+    }else{
+      const formData=this.registerForm.getRawValue();
+      const obj={
+        "id": this.editFlag ? this.editObj?.id :0,
+        "organizationId": formData?.organizationId || 0,
+        "partyId": formData?.employeeId|| 0,
+        "visitDate": this.commonService.dateFormat(formData?.visitDate,'dateWithTime'),
+        "visitTime": formData?.visitTime || 0,
+        "fromUnitId": formData?.fromUnitId || 0,
+        "toUnitId": formData?.unitId || 0,
+        "visitDetails": formData?.visitDetails,
+        "createdBy": this.webService.createdByProps().createdBy,
+        "modifiedBy": this.webService.createdByProps().modifiedBy
+      }
+      const str = 'api/VisitEntry/' + (this.editFlag ? 'UpdateVisitEntry' : 'AddVisitEntry');
+        this.apiService.setHttp(this.editFlag ? 'put' : 'post', str, false, obj, false, 'priyadarshaniService');
+        this.apiService.getHttp().subscribe({
+          next: ((res: any) => {
+            if (res?.statusCode == 200) {
+              this.commonService.matSnackBar(res?.statusMessage, 0);
+              this.resetForm('Yes');
+            }
+            else {
+              this.commonService.checkDataType(res?.statusMessage) == false ? this.errorService.handleError(res?.statusCode) : this.commonService.matSnackBar(res?.statusMessage, 1);
+            }
+          }),
+          error: (error: any) => {
+            this.errorService.handleError(error.status);
+          }
+        })
+    }
+  }
+
+  resetForm(flag?:any){
+    this.formDirective && this.formDirective.resetForm();
+    this.commonService.routerLinkRedirect('/visit-entry-list');
+  }
 }
--- src/app/modules/secure/visit-entry-list/visit-entry-list.component.ts
+++ src/app/modules/secure/visit-entry-list/visit-entry-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -7,7 +7,16 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
+import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
+import { ApiService } from 'src/app/core/services/api.service';
+import { ErrorService } from 'src/app/core/services/error.service';
+import { FormBuilder, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { ReplaySubject, Subject, take, takeUntil } from 'rxjs';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { MasterService } from 'src/app/core/services/master.service';
+import { AESEncryptDecryptService } from 'src/app/core/services/aesencrypt-decrypt.service';
 
 @Component({
   selector: 'app-visit-entry-list',
@@ -21,11 +30,193 @@ import { RouterModule } from '@angular/router';
     MatInputModule,
     MatDatepickerModule,
     MatSelectModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxMatSelectSearchModule
   ],
   templateUrl: './visit-entry-list.component.html',
   styleUrls: ['./visit-entry-list.component.scss']
 })
 export class VisitEntryListComponent {
 
+  visitEntryArray = new Array();
+  tableDatasize!: number;
+  totalPages!: number;
+  pageNumber: number = 1;
+  pageSize: number = 10;
+  filterForm!: FormGroup;
+  get f(){ return this.filterForm.controls};
+  @ViewChild('formDirective') private formDirective!: NgForm;
+
+  organizationArray = new Array();
+  orgCtrl: FormControl = new FormControl(); 
+  orgSubject: ReplaySubject<any> = new ReplaySubject<any>();
+
+  allUnits = new Array();
+  unitCtrl: FormControl = new FormControl();
+  unitSubj: ReplaySubject<any> = new ReplaySubject<any>();
+
+  employeeArray = new Array();
+  employeeCtrl: FormControl = new FormControl();
+  employeeSubject: ReplaySubject<any> = new ReplaySubject<any>();
+
+  tooltipMessage = 'Select All / Unselect All';
+  _onDestroy = new Subject<void>();
+  isChecked: boolean = false;
+  currentDate=new Date()
+
+  constructor(private webService: WebStorageService,
+    private fb: FormBuilder,
+    private commonMethod: CommonMethodsService,
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute,
+    private AESEncryptDecryptService: AESEncryptDecryptService,
+    private masterService: MasterService,
+    private errorService: ErrorService) {
+      this.activatedRoute.params.subscribe(params => {
+        this.getLoggedInUserDetails(params['id']);
+      });
+    }
+
+  ngOnInit(){
+    this.createFilterForm();
+    this.getOrganization();
+    //this.getTableDetails();
+  }
+
+  getLoggedInUserDetails(mNo: any){
+    var req = { mobileNo: mNo };
+    this.apiService.setHttp('POST', 'Login/UserLoginBymobile', false, req, false, 'priyadarshaniService');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => {
+          if (res.statusCode == 200) {
+            sessionStorage.setItem('loggedIn', 'true');            
+            var loginData = this.AESEncryptDecryptService.encrypt(JSON.stringify(res?.responseData));            
+            localStorage.setItem('mobileUserLoggedInData', loginData);
+          } else {
+            this.commonMethod.matSnackBar(res.statusMessage, 1)
+          }
+        },
+        error: (err: any) => {
+          this.errorService.handleError(err.status);
+        },
+      });
+  }
+
+  createFilterForm(){
+    this.filterForm = this.fb.group({
+      organization: [],
+      unit: [],
+      fromDate: [new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), 1)],
+      toDate: [new Date()],
+      employeeList: [],
+    })
+  }
+
+  getOrganization(obj?:any) {
+    this.masterService.getAllOrganizationByUserId(this.webService.getUserId()).subscribe({
+      next: ((res: any) => {
+        if (res.statusCode == "200") {
+          this.organizationArray = res.responseData;
+          this.commonMethod.filterArrayDataZone(this.organizationArray,this.orgCtrl,'organizationName',this.orgSubject);
+          this.webService.getOrgId() || obj?.organization ? (this.f['organization'].setValue(this.webService.getOrgId() || obj?.organization), this.dropdownClickEvent('org')) : '';
+          
+        }
+        else {
+          this.clearDependancy('org');
+        }
+      }),
+      error: () => { this.clearDependancy('org'); }
+
+    })
+  }
+
+  getUnit(obj?: any) {
+    const orgId = this.f['organization'].getRawValue();
+    this.masterService.getAllUnitByOrgIdUserId(orgId, this.webService.getUserId()).subscribe({
+      next: ((res: any) => {
+        if (res.statusCode == "200") {
+          this.allUnits = res.responseData;
+          this.commonMethod.filterArrayDataZone(this.allUnits, this.unitCtrl, 'unitName', this.unitSubj);
+          (this.webService.getUnitId() || obj?.unit) ? (this.f['unit'].setValue(this.webService.getUnitId() || obj?.unit), this.dropdownClickEvent('unit')) : '';
+
+        }
+        else {
+          this.clearDependancy('unit');
+        }
+      }), error: () => { this.clearDependancy('unit'); }
+    })
+  }
+
+  getEmployeeDetails() {
+    this.masterService.getAllEmployeeByOrg((this.f['organization'].getRawValue() || 0)).subscribe({
+      next: ((res: any) => {
+        if (res.statusCode == "200") {
+          this.employeeArray = res.responseData;
+          this.commonMethod.filterArrayDataZone(this.employeeArray, this.employeeCtrl, 'employeeName', this.employeeSubject);
+        }
+        else {
+          this.clearDependancy('employee');
+        }
+      }), error: () => { this.clearDependancy('employee'); }
+    })
+  }
+
+  getTableDetails(flag?: any) {
+    if(this.filterForm.invalid){
+      return;
+    }else{
+      const formData = this.filterForm.getRawValue();
+      let str = ('pageno=' + +(this.pageNumber || 0) + '&pagesize=' + 10 + '&UnitId=' + (formData?.unit || 0) + '&PartyIds=' + (formData?.employeeList?.toString() || '') + '&fromDate=' + (this.commonMethod.dateFormat(formData?.fromDate, 'year')) +
+        '&organizationId=' + (formData?.organization || 0) + '&toDate=' + (this.commonMethod.dateFormat(formData?.toDate, 'year')));
+      this.apiService.setHttp('GET', 'api/VisitEntry/GetAllVisitEntries?' + str, false, false, false, 'priyadarshaniService');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => {
+          if (res.statusCode == 200) {
+            this.visitEntryArray = res.responseData;
+            this.tableDatasize = res.responseData1?.totalCount;
+            this.totalPages = res.responseData1?.totalPages;
+
+          } else {
+            this.commonMethod.checkDataType(res.statusMessage) == false
+              ? this.errorService.handleError(res.statusCode)
+              : '';
+            this.visitEntryArray = [];
+            this.tableDatasize = 0;
+          }
+        },
+        error: (err: any) => {
+          this.errorService.handleError(err.status);
+        },
+      });
+    }
+  }
+
+  toggleSelectAll(selectAllValue: boolean, flag: any) {    
+    var arr = this.employeeArray.map((x: any) => x.id)
+    if(flag != 'Single'){
+      this.employeeSubject
+        .pipe(take(1), takeUntil(this._onDestroy))
+        .subscribe(() => {
+          this.f['employeeList'].patchValue(selectAllValue ? [...arr] : []);
+      });
+    }else{
+      this.isChecked = ((arr.length != this.f['employeeList'].value.length) ? false : true);
+    }
+  }
+  dropdownClickEvent(_status?:any){
+
+  }
+
+  clearDependancy(flag: any){
+    switch(flag){
+      case 'org': this.allUnits = [], this.unitSubj.next(null), this.employeeArray = [], this.employeeSubject.next(null);
+      break;
+    }
+  }
+
+  redirectToAddVisitEntry(){
+    this.commonMethod.routerLinkRedirect('/add-visit-entry');
+  }
 }
