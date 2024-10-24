2046eae810ad625b025691488f8c01bb18734620
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2024-10-24 14:39:56 +0530

    delete confirmation dialog box designed

--- src/app/shared/components/confirmation-dialog-t1/confirmation-dialog-t1.component.ts
+++ src/app/shared/components/confirmation-dialog-t1/confirmation-dialog-t1.component.ts
@@ -1,11 +1,18 @@
 import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-confirmation-dialog-t1',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './confirmation-dialog-t1.component.html',
   styleUrls: ['./confirmation-dialog-t1.component.scss']
 })
